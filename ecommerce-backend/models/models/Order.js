ff9e5044b75bc5e73d0882157f5052ebf56104ac
Gguptarohit <rg0646128@gmail.com>
2025-07-23 21:39:19 +0530

    Update Order.js

--- ecommerce-backend/models/models/Order.js
+++ ecommerce-backend/models/models/Order.js
@@ -1,1 +1,11 @@
 
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  productId: String,
+  quantity: Number,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
